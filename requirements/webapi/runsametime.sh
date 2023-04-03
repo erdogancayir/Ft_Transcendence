28453f76f15c4610276ce3942a3a761f3c667081
ErdoganCayir <erdogancayir@pop-os.localdomain>
2023-04-03 05:24:04 +0300

    ok

--- requirements/webapi/runsametime.sh
+++ requirements/webapi/runsametime.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /home/omeryumusak/webapi && npm install -g prisma -y && npx prisma migrate dev --name dev && npm run start &
+cd /home/erdogancayir/webapi && npm install -g prisma -y && npx prisma migrate dev --name dev && npm run start &
 /usr/sbin/sshd -D 
